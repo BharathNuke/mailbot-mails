d9b70f6886af9db74d9a05ce9f8a6607d221e3d9
DataFire <info@datafire.io>
2019-02-04 13:03:13 +0518

    added parse_feeds

---
+++ actions/parse_feeds.js
@@ -0,0 +1,22 @@
+"use strict";
+let datafire = require('datafire');
+
+module.exports = new datafire.Action({
+  description: "parse array of feeds to mail body",
+  inputs: [{
+    type: "object",
+    title: "feeds"
+  }],
+  handler: async (input, context) => {    
+    let body = '';
+    input.feeds.forEach(result => {
+      let feed = result.feed;
+      body += `<h1>${feed.title}</h1>`;
+      let entries = feed.entries.slice(0, 8);
+      entries.forEach(entry => {
+        body += `<p><a href="${entry.link}">${entry.title}</a></p>`
+      })
+    });
+    return body;
+  },
+});
